eb2fd316ccf7a8fad830756b38fdfceed5b296c8
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-21 14:04:55 +0100

    Test initializing Oystercard with balance passing

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,13 +1,13 @@
-require 'oyster'
+require "oyster"
 
 describe Oystercard do
-  it 'creates an instance of oystercard' do
+  it "creates an instance of oystercard" do
     expect(subject).to be_an_instance_of Oystercard
     # it {is_expected.to be_an_instance of Oystercard}
   end
   describe "#initialize" do
     it "initializes an Oystercard with a balance of 0" do
-        expect(subject.balance).to eq 0
+      expect(subject.balance).to eq 0
     end
-    end
+  end
 end
