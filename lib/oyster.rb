8815df550acf5ce31ede604c81acbbaece99d326
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-22 08:59:01 +0100

    ignore

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,2 +1,16 @@
 class Oystercard
+
+  def top_up(topup)
+    raise "Exceeds maximum limit of 90" if @balance + topup > 90
+    @balance = @balance + topup
+  end
+
+  def limit(topup)
+    @balance + topup > 90
+  end
+
+  def empty?(duduct)
+    @balance = @balance - deduct < 0
+  end
+
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -5,9 +5,10 @@ describe Oystercard do
     expect(subject).to be_an_instance_of Oystercard
     # it {is_expected.to be_an_instance of Oystercard}
   end
+  
   describe "#initialize" do
     it "initializes an Oystercard with a balance of 0" do
         expect(subject.balance).to eq 0
-    end
+      end
     end
 end
